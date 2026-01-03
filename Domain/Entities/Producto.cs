cbe22536179cd2b913e4a37dca7b41b8790777ab
roci000 <roci.tarifa.a123@gmail.com>
2026-01-03 15:38:26 -0400

    Domain: Producto y Proveedor

---
+++ Domain/Entities/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Producto
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string UnidadMedida { get; set; } = string.Empty; 
+        public decimal PrecioReferencia { get; set; }
+        public int StockActual { get; set; }
+        public bool Estado { get; set; } = true; 
+    }
+}
---
+++ Domain/Entities/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Proveedor
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Telefono { get; set; } 
+        public string Direccion { get; set; } = string.Empty;
+        public bool Estado { get; set; } = true;
+    }
+}
