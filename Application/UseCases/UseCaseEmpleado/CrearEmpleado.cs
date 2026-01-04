59742963af4ca064e2238abb1889ce7e3ab313e4
roci000 <roci.tarifa.a123@gmail.com>
2026-01-04 15:02:11 -0400

    Casos de Uso para Empleado

---
+++ Application/UseCases/UseCaseEmpleado/CrearEmpleado.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.UseCaseEmpleado
+{
+    public class CrearEmpleado
+    {
+        private readonly IEmpleado _empleado;
+        public CrearEmpleado(IEmpleado empleado)
+        {
+            _empleado = empleado;
+        }
+
+        public async Task EjecutarAsync(Empleado empleado)
+        {
+            ValidarEmpleado(empleado);
+            await _empleado.Crear(empleado);
+        }
+
+        private void ValidarEmpleado(Empleado empleado)
+        {
+            if (string.IsNullOrWhiteSpace(empleado.Nombre))
+                throw new ArgumentException("El nombre del empleado es obligatorio.");
+
+            if (string.IsNullOrWhiteSpace(empleado.Apellido))
+                throw new ArgumentException("El apellido del empleado es obligatorio.");
+        }
+    }
+}
---
+++ Application/UseCases/UseCaseEmpleado/EliminarEmpleado.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.UseCaseEmpleado
+{
+    public class EliminarEmpleado
+    {
+        private readonly IEmpleado _empleado;
+
+        public EliminarEmpleado(IEmpleado empleado)
+        {
+            _empleado = empleado;
+        }
+
+        public async Task EjecutarAsync(Guid id)
+        {
+            if (id == Guid.Empty)
+                throw new ArgumentException("El ID del empleado es inválido.");
+
+            await _empleado.Eliminar(id);
+        }
+    }
+}
