329d66ded60f79616d4375f31a7409feb202c681
roci000 <roci.tarifa.a123@gmail.com>
2026-01-05 13:51:36 -0400

    Entidad y Interface de Venta

---
+++ Domain/Interfaces/IVenta.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IVenta
+    {
+        Task<IEnumerable<Venta>> All();
+        Task<Venta?> ObtenerPorId(Guid id);
+        Task Crear(Venta venta);
+        Task AnularVenta(Guid id, string motivo);
+    }
+}
